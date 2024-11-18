4d2c085393b074e7ef990f6b306d6184e338742c
TheoristMC <zunheorist@gmail.com>
2024-11-18 12:10:59 +0800

    Prerelease - Added shooting with spacebar.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Space-Blaster
 
-This is a web-game which is based on Javascript that is made by 14 year old to test his skill. Play the game on - [Not Available](https://github.com/TheoristMC/Space-Blaster)
+This is a web-game which is based on Javascript that is made by 14 year old to test his skill. Play the game on - [Space-Blaster](https://theoristmc.github.io/Space-Blaster/)
 
 ## Tutorial
 
-Move the player with arrow-keys! Only supports PC by now.
+Move the spaceship with arrow-keys and press spacebar to blast! Only supports PC by now.
--- space/main.js
+++ space/main.js
@@ -2,17 +2,78 @@ const GameBoundary = document.getElementById('gameBoundary');
 
 const GameRule = {
   scrollBgSpd: 0.4,
+  spaceshipSpd: 5,
+  spaceshipX: 0,
   bgY: 0
+};
+
+function spawnAndShootBullets() {
+  const spaceship = document.getElementById('spaceship');
+
+  const bullet = document.createElement('div');
+  bullet.classList.add('isBullet');
+
+  spaceship.appendChild(bullet);
+
+  // Shoot the bullet
+  let bulletY = 0;
+
+  const bulletInterval = setInterval(() => {
+    bulletY += -5;
+    bullet.style.transform = `translateY(${bulletY}px)`;
+
+    if (Math.abs(bulletY) > (GameBoundary.clientHeight - spaceship.clientHeight - bullet.clientHeight)) {
+      clearInterval(bulletInterval);
+      bullet.remove();
+    }
+  }, 16); // Approx. 60 fps
+};
+
+function moveShip(direction) {
+  const spaceship = document.getElementById('spaceship');
+
+  const newSpaceshipX = GameRule.spaceshipX + GameRule.spaceshipSpd * direction;
+
+  if (newSpaceshipX >= 0 && newSpaceshipX <= (GameBoundary.clientWidth - spaceship.clientWidth)) {
+    GameRule.spaceshipX = newSpaceshipX;
+    spaceship.style.transform = `translateX(${GameRule.spaceshipX}px)`;
+  }
+};
+
+function spawnShip() {
+  const spaceship = document.createElement('div');
+  spaceship.id = 'spaceship';
+
+  GameBoundary.appendChild(spaceship);
 };
 
 function scrollBackground() {
   GameRule.bgY += GameRule.scrollBgSpd;
   GameBoundary.style.backgroundPositionY = `${GameRule.bgY}px`;
   requestAnimationFrame(scrollBackground);
-}
+};
+
+document.addEventListener('keydown', (ev) => {
+  let direction = 0;
+
+  switch (ev.code.toString().toUpperCase()) {
+    case 'ARROWRIGHT':
+      direction = 1;
+      break;
+    case 'ARROWLEFT':
+      direction = -1;
+      break;
+    case 'SPACE':
+      spawnAndShootBullets();
+      break;
+  }
+
+  moveShip(direction);
+});
 
 function startup() {
   scrollBackground();
+  spawnShip();
 };
 
 startup();
